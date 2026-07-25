dd7ab9a7f58a44c6639cb0bcbfbbf442f561d1a5
Prathamesh Salokhe <146345504+JACKSPARROW10-MAX@users.noreply.github.com>
2026-07-25 20:55:38 +0530

    Time: 3 ms (46.15%), Space: 12.4 MB (16.08%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -1,13 +1,6 @@
 class Solution(object):
     def maxProduct(self, n):
-        p=str(n)
-        arr=[]
-        for i in p:
-            arr.append(int(i))
-        print(arr)
-        m1=max(arr)
-        arr[arr.index(m1)]=0
-        m2=max(arr)
-        return m1*m2
+     p=sorted(str(n))
+     return int(p[-1])*int(p[-2])
 
         
