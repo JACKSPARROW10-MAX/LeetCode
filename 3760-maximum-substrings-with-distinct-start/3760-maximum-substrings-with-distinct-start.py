3cd23bca788bd5781679bfd4d6a866f4146796b7
Prathamesh Salokhe <146345504+JACKSPARROW10-MAX@users.noreply.github.com>
2026-08-08 10:49:03 +0530

    Time: 27 ms (73.98%), Space: 12.9 MB (73.79%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
@@ -1,11 +1,4 @@
 class Solution(object):
     def maxDistinct(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ans=set()
-        for i in s:
-            ans.add(i)
-        return len(ans)
+        return len(set(s))
         
