063351b03350931abb222bb97e9ecac5ffde9261
Prathamesh Salokhe <146345504+JACKSPARROW10-MAX@users.noreply.github.com>
2026-09-14 23:40:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.16%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -0,0 +1,4 @@
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        (x1, y1, x2, y2), (X1, Y1, X2, Y2)=rec1,rec2
+        return x1 < X2 and X1 < x2 and y1 < Y2 and Y1 < y2
